31a41f8b3dcbf12b0dc2e67e135afc9a5f67050b
Stefan Velkovski <svelkovski67@gmail.com>
2026-03-22 00:06:21 +0100

    Make header component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgMain: '#0F1115',
+        card: '#1C1F26',
+        textPrimary: '#FFFFFF',
+        textSecondary: '#A0A0A0',
+        accentRed: '#E63946',
+        borderGray: '#2C2F38',
+        rankUp: '#4ADE80',
+        rankDown: '#F87171',
+      },
+    },
   },
   plugins: [],
-};
+}
