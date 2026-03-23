e246aac27e952c07cc09355973d3f900dc51fa06
Stefan Velkovski <svelkovski67@gmail.com>
2026-03-23 03:51:11 +0100

    Create rankings component

---
+++ src/app/rankings/rankings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rankings',
+  standalone: true,
+  imports: [],
+  templateUrl: './rankings.component.html',
+  styleUrl: './rankings.component.css'
+})
+export class RankingsComponent {
+
+}
