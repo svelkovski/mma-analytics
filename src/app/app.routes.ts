c8dfee74e1185b3102365c56d851afb56c88d481
Stefan Velkovski <svelkovski67@gmail.com>
2026-03-23 04:54:50 +0100

    Add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FightersComponent } from './fighters/fighters.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: DashboardComponent,
+  },
+  {
+    path: 'fighters',
+    component: FightersComponent,
+  },
+];
