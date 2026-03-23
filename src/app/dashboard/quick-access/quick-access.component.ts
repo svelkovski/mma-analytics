14937c45b8c4d72e6a1a49eb0b75a0294392a5d1
Stefan Velkovski <svelkovski67@gmail.com>
2026-03-23 05:01:03 +0100

    Add router link

--- src/app/dashboard/quick-access/quick-access.component.ts
+++ src/app/dashboard/quick-access/quick-access.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-quick-access',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './quick-access.component.html',
   styleUrl: './quick-access.component.css'
 })
