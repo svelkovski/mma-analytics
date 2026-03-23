94694a99754c9ef179e75857a82c3b5ae01e05dd
Stefan Velkovski <svelkovski67@gmail.com>
2026-03-23 04:55:36 +0100

    Modify hero-section design

--- src/app/dashboard/hero-section/hero-section.component.ts
+++ src/app/dashboard/hero-section/hero-section.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.css'
 })
