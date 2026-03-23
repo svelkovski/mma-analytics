389e1658ddb709d216b5979d9c2d5169bf0f0e00
Stefan Velkovski <svelkovski67@gmail.com>
2026-03-23 04:56:15 +0100

    Implement fighter details functionality

--- src/app/fighters/fighter-details/fighter-details.component.ts
+++ src/app/fighters/fighter-details/fighter-details.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { Fighter } from '../fighter.model';
 
 @Component({
   selector: 'app-fighter-details',
   standalone: true,
   imports: [],
   templateUrl: './fighter-details.component.html',
-  styleUrl: './fighter-details.component.css'
+  styleUrl: './fighter-details.component.css',
 })
 export class FighterDetailsComponent {
+  fighter = input.required<Fighter>();
+  close = output<boolean>();
 
+  onCloseDetails() {
+    this.close.emit(false);
+  }
 }
--- src/app/fighters/fighters.component.ts
+++ src/app/fighters/fighters.component.ts
@@ -1,16 +1,19 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { Fighter } from './fighter.model';
 import { FightersService } from './fighters.service';
+import { FighterDetailsComponent } from './fighter-details/fighter-details.component';
 
 @Component({
   selector: 'app-fighters',
   standalone: true,
-  imports: [],
+  imports: [FighterDetailsComponent],
   templateUrl: './fighters.component.html',
   styleUrl: './fighters.component.css',
 })
 export class FightersComponent implements OnInit {
   fighters = signal<Fighter[] | undefined>(undefined);
+  selectedFighter!: Fighter;
+  isViewingDetails = false;
 
   private fightersService = inject(FightersService);
 
@@ -18,5 +21,14 @@ export class FightersComponent implements OnInit {
     this.fightersService.fighters$.subscribe((fighters) =>
       this.fighters.set(fighters),
     );
+  }
+
+  onViewDetails(fighter: Fighter) {
+    this.selectedFighter = fighter
+    this.isViewingDetails = true;
+  }
+
+  onCloseDetails() {
+    this.isViewingDetails = false;
   }
 }
