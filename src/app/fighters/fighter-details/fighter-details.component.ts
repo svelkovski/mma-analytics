c8351d7e243b7bde57f58f049eb96d95c3d07617
Stefan Velkovski <svelkovski67@gmail.com>
2026-03-23 03:51:35 +0100

    Create fighter-details component

---
+++ src/app/fighters/fighter-details/fighter-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fighter-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './fighter-details.component.html',
+  styleUrl: './fighter-details.component.css'
+})
+export class FighterDetailsComponent {
+
+}
