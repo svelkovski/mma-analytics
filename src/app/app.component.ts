35874bab7c5a1cc00aeb5d002b56e0d48140b381
Stefan Velkovski <svelkovski67@gmail.com>
2026-03-23 05:00:41 +0100

    Update imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { FooterComponent } from './footer/footer.component';
 import { RankingsService } from './rankings/rankings.service';
-import { FightersComponent } from "./fighters/fighters.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, DashboardComponent, FightersComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  private ranking = inject(RankingsService)
+  private ranking = inject(RankingsService);
 }
